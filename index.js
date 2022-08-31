1e4a52ccffac3b88a798789d2c7afa8f3c59cc68
unknown <shahidmonowar.cse@gmail.com>
2022-08-31 16:36:23 +0600

    database connected

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
@@ -8,6 +9,17 @@ const app = express();
 //middleware
 app.use(cors());
 app.use(express.json());
+
+//database connection
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.3bc4k.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  console.log('agency db connected');
+  // perform actions on the collection object
+  client.close();
+});
+
 
 app.get('/',(req, res)=>{
     res.send('Running agency server');
