e62366bc76a67575f4d5d57debb67b1bd35ed0de
unknown <shahidmonowar.cse@gmail.com>
2022-09-01 19:15:57 +0600

    get and post course api

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
@@ -12,19 +12,47 @@ app.use(express.json());
 
 //database connection
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.3bc4k.mongodb.net/?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  console.log('agency db connected');
-  // perform actions on the collection object
-  client.close();
+console.log(uri);
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
 });
 
+async function run() {
+  try {
+    await client.connect();
+    const database = client.db('agency');
+    const courseCollection = database.collection('course');
 
-app.get('/',(req, res)=>{
-    res.send('Running agency server');
+    //get api
+    app.get('/course', async (req, res) => {
+      const cursor = courseCollection.find({});
+      const courses = await cursor.toArray();
+      res.send(courses);
+  })
+
+  //Post api
+  app.post('/course', async (req, res) => {
+    const course = req.body;
+    console.log('hit the post api', course);
+
+    const result = await courseCollection.insertOne(course);
+
+    console.log(result);
+    res.send(result);
+}) 
+  } finally {
+    //await client.close();
+  }
+}
+
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Running agency server");
 });
 
-app.listen(port, ()=>{
-    console.log('listening to port',port);
+app.listen(port, () => {
+  console.log("listening to port", port);
 });
