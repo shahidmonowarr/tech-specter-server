00d0665b834bedebf3c1fcf6d69d4fe3fcf62740
unknown <shahidmonowar.cse@gmail.com>
2022-09-14 15:39:17 +0600

    load all users, make admin & admin level access

--- index.js
+++ index.js
@@ -55,6 +55,37 @@ async function run() {
     //   res.send({accessToken});
     // });
 
+    app.get('/user', verifyJWT, async (req, res)=>{
+      const users = await userCollection.find().toArray();
+      res.send(users);
+    });
+
+    app.get('/admin/:email', async (req, res) =>{
+      const email = req.params.email;
+      const user = await userCollection.findOne({email: email});
+      const isAdmin = user.role === 'admin';
+      res.send({admin: isAdmin});
+    })
+
+    app.put("/user/admin/:email", verifyJWT, async (req, res) => {
+      const email = req.params.email;
+      const requester = req.decoded.email;
+      const requesterAccount = await userCollection.findOne({email: requester});
+      if(requesterAccount.role === 'admin'){
+        const filter = { email: email };
+      const updateDoc = {
+        $set: {role: 'admin'},
+      };
+      const result = await userCollection.updateOne(filter, updateDoc);
+      res.send(result);
+      }
+      else{
+        res.status(403).send({message: 'Forbidden'});
+      }
+      
+    });
+
+
     app.put("/user/:email", async (req, res) => {
       const email = req.params.email;
       const user = req.body;
